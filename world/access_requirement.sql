587a79973b5cb9faee28a5ca821a9620df477695
Huri <Huricane_20@web.de>
2014-08-24 16:50:26 +0200

    Übersetzungen für die Tabelle `access_requirement` hinzugefügt

---
+++ world/access_requirement.sql
@@ -0,0 +1,5 @@
+﻿-- Schwarzer Morast (Normal) +  (Heroisch)
+-- Original `quest_failed_text`: You must complete the quest "Return to Andormu" before entering the Black Morass.
+UPDATE `access_requirement` SET `quest_failed_text` = 'Ihr müsst die Quest "Rückkehr zu Andormu" abgeschlossen haben, bevor ihr den Schwarzen Morast betreten könnt.' WHERE `mapId` = 269 and `difficulty` = 0;
+-- Original `quest_failed_text`: You must complete the quest "Return to Andormu" and be level 70 before entering the Heroic difficulty of the Black Morass.
+UPDATE `access_requirement` SET `quest_failed_text` = 'Ihr müsst die Quest "Rückkehr zu Andormu" abgeschlossen und das Level 70 erreicht haben, bevor ihr den Schwarzen Morast (Heroisch) betreten könnt.' WHERE `mapId` = 269 and `difficulty` = 1;
